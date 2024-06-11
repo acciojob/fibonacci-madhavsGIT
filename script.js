2d05bec401ebc61fa578a5436a9ca4c37064fa58
madhav <163291389+madhavsGIT@users.noreply.github.com>
2024-06-11 14:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fibonacci(num) {
 // your code here
-		if(num === 1 || num === 0) return num;
-	    
+		if(num === 1 || num === 0) return 0;
+	    if(num === 2) return 1;
 		return fibonacci(num -1) + fibonacci(num - 2);
 }
 
